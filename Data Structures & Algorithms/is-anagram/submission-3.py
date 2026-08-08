24f9f890256c5b4bae176371fe69489c48ad2664
Y-KEBEDE19 <kebedeyididya24@gmail.com>
2026-08-08 11:31:33 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        mapS = collections.defaultdict(int)
+        mapT = collections.defaultdict(int)
+
+        if len(s) != len(t):
+            return False
+        
+        for i in range(len(s)):
+            if s[i] in mapS:
+                mapS[s[i]] += 1
+            else:
+                mapS[s[i]] = 1
+            
+            if t[i] in mapT:
+                mapT[t[i]] += 1
+            else:
+                mapT[t[i]] = 1
+
+        return mapS == mapT
